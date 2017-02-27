e32b8f83d7010f881220b15f32e41c7235207be2
alexKwonIsAwesome <khwalex@gmail.com>
2017-02-27 17:17:05 +0900

    insert icon in serach bar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import magnifyingGlass from '../../public/magnifying-glass.svg';
 
 const SearchBar = () => (
   <form>
     <input className="search-bar grid-50" placeholder="원하는 짤을 찾아보세요!" />
-    <img alt="magnifying glass for search" src="image/magnifying-glass.svg"/>
+    <img alt="magnifying glass for search" src={magnifyingGlass}/>
   </form>
 )
 
