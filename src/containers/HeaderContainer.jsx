39966d375a42106ce575d1f507753ae2a69c0f35
alexKwonIsAwesome <khwalex@gmail.com>
2017-02-27 12:01:33 +0900

    add switcher depending on auth state

--- src/containers/HeaderContainer.jsx
+++ src/containers/HeaderContainer.jsx
@@ -3,6 +3,30 @@ import logo from '../../public/logo.svg';
 import { connect } from 'react-redux';
 
 class HeaderContainer extends Component {
+  switchPane() {
+    if (this.props.isLoggedIn) {
+      return (
+        <div id="nav-button" className="auth">
+          <div className="nav-button-group">
+            <a href="#" className="btn btn-primary" id="jjal-upload">짤 업로드</a>
+            <a href="#" className="btn btn-secondary">내 줍줍</a>
+          </div>
+          <a href="#" id="profile-image">
+            <img alt="user-profile" className="image-circle" src="image/facebook-profile-2.jpg"/>
+          </a>
+        </div>
+      )
+    }
+
+    return (
+      <div id="nav-button">
+        <a href="#" className="btn btn-primary" id="jjal-upload">짤 업로드</a>
+        <a href="#" className="btn btn-secondary btn-seperation">로그인</a>
+        <a href="#" className="btn btn-secondary">가입</a>
+      </div>
+    )
+  }
+
   render() {
     return (
       <header className="grid-container">
@@ -15,11 +39,7 @@ class HeaderContainer extends Component {
                 <li><a href="#">하태핫태</a></li>
               </ul>
             </div>
-            <div id="nav-button">
-              <a href="#" className="btn btn-primary" id="jjal-upload">짤 업로드</a>
-              <a href="#" className="btn btn-secondary btn-seperation">로그인</a>
-              <a href="#" className="btn btn-secondary">가입</a>
-            </div>
+            {this.switchPane()}
         </div>
     </header>
     );
