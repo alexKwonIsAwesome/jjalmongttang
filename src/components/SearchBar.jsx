898e8653b4b098414c811fc5030e6bb10a9814e8
alexKwonIsAwesome <khwalex@gmail.com>
2017-02-27 12:22:59 +0900

    extract search bar from jumbotron

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const SearchBar = () => (
+  <form>
+    <input className="search-bar grid-50" placeholder="원하는 짤을 찾아보세요!" />
+    <img src="image/magnifying-glass.svg"/>
+  </form>
+)
+
+export default SearchBar;
