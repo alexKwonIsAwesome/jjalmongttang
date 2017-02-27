cafac30c5c8ab52412fd1d244a87775dc26009d1
alexKwonIsAwesome <khwalex@gmail.com>
2017-02-27 17:26:27 +0900

    init JjalInstance component:
    this component is considered as a single page and rendered through
    router. It is going to contain the single instance of jjal.

---
+++ src/components/JjalInstance.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import HeaderContainer from '../containers/HeaderContainer';
+
+const JjalInstance = () => (
+  <div>
+    <HeaderContainer />
+  </div>
+)
+
+export default JjalInstance;
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 
 import Home from '../components/Home';
+import JjalInstance from '../components/JjalInstance';
 
 export default (
-    <Route path="/" component={Home}>
-      <IndexRoute />
+    <Route path="/">
+      <IndexRoute component={Home} />
+      <Route path="jjal" component={JjalInstance} />
     </Route>
 );
